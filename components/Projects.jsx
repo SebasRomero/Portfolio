ebcf1b9cdb5b2665f9c361b409cecee1748799cb
sebasromero <sebastian_eromero@hotmail.com>
2023-04-24 23:24:45 -0400

    Improving the Skills component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <div className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-xl tracking-widest uppercase text-teal-300">
+        <p className="text-2xl tracking-widest uppercase text-teal-300">
           Projects
         </p>
         <h2 className="py-4">What I've Built</h2>
---
+++ components/SkillItem.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const SkillItem = (props) => {
+  return (
+    <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={props.image}
+                  width={props.width}
+                  height={props.height}
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>{props.tittle}</h3>
+              </div>
+            </div>
+          </div>
+  )
+}
+
+export default SkillItem
