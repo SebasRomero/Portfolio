f1b30c1a0f71e77e0038ecc69253395ec617d1c3
sebasromero <sebastian_eromero@hotmail.com>
2023-07-08 22:48:09 -0400

    feat: add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [{
+        script: 'npm start'
+    }],
+
+    deploy: {
+        production: {
+            key: 'key.pem',
+            user: 'ubuntu',
+            host: '54.86.202.11',
+            ref: 'origin/main',
+            repo: 'https://github.com/SebasRomero/Portfolio.git',
+            path: '/home/ubuntu',
+            'pre-deploy-local': '',
+            'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': '',
+        }
+    }
+}
